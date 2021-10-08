fdfc7c6e5f21527f7c2492c01c636832a493a35a
Rafael Henrique Nogalha de Lima <64447809+rafaelnogalha@users.noreply.github.com>
2021-10-08 10:38:06 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ import mysql.connector as connector
 
 config = {
   'user': 'root',
-  #'password': 'L@g0n1c0',
-  'password': 'mysql12',
+  #'password': 
+  'password': '',
   'host': 'localhost',
   'database': 'redeSocial' # tirar para estabelecer conexao, colocar para acessar o baanco de dados
 }
